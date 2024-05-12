f5196d3e6c3a4d5e0ae3112c8ac82162039e9029
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-12 00:46:16 -0300

    Coloquei um texto para teste

--- templates/header.php
+++ templates/header.php
@@ -14,6 +14,8 @@
     </head>
 
     <body> 
+        <h1>Teste</h1>
+        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu aliquet lacus. Suspendisse vel lectus sed nulla tempus congue eu eget enim. Duis leo odio, convallis id scelerisque vitae, dignissim ac felis. Curabitur sagittis ullamcorper pellentesque. Donec ac ipsum dui. Maecenas pulvinar ipsum sed erat euismod pharetra. Ut nisl metus, suscipit et erat at, cursus suscipit odio.</h2>
 
     <header>
     </hader>
